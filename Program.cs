9454af0c96e750823873e163f3a5ef4f67a82a25
kimilao01 <76526337+kimilao01@users.noreply.github.com>
2020-12-24 07:39:08 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter a number: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                string binary = Convert.ToString(num, 2);
+                Console.WriteLine(binary);
+            }
+        }
+    }
+}
